da9ba6f1fb4c96893b407f5fd09de9fc9b62f419
ycy <3240056286@qq.com>
2020-06-25 17:44:38 +0800

    新增文件

---
+++ gitstudy/src/main/java/com/yan/pojo/Books.java
@@ -0,0 +1,64 @@
+package com.yan.pojo;
+/**
+* @author ycy
+* @version 1.0
+* @createDate 创建时间：2020/05/17 15:40:20
+* @explain(类说明):...
+*/
+public class Books {
+    
+	private int bookID;
+	private String bookName;
+	private int bookCounts;
+	private String detail;
+	
+	public Books() {
+		super();
+	}
+	
+	public Books(int bookID, String bookName, int bookCounts, String detail) {
+		super();
+		this.bookID = bookID;
+		this.bookName = bookName;
+		this.bookCounts = bookCounts;
+		this.detail = detail;
+	}
+
+	public int getBookID() {
+		return bookID;
+	}
+
+	public void setBookID(int bookID) {
+		this.bookID = bookID;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public int getBookCounts() {
+		return bookCounts;
+	}
+
+	public void setBookCounts(int bookCounts) {
+		this.bookCounts = bookCounts;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	@Override
+	public String toString() {
+		return "Books [bookID=" + bookID + ", bookName=" + bookName + ", bookCounts=" + bookCounts + ", detail="
+				+ detail + "]";
+	}
+}
