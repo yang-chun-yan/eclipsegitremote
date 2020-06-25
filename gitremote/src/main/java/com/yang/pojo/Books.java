b9692767177ff34ebf9fcf1ace240f5dd97f70b0
ycy <3240056286@qq.com>
2020-06-25 22:43:18 +0800

    第一次提交

--- gitremote/src/main/java/com/yang/pojo/Books.java
+++ gitremote/src/main/java/com/yang/pojo/Books.java
@@ -9,7 +9,7 @@ public class Books {
     
 	private String bookUser;
 	private int bookID;
-	private String bookName;
+	private String bookName;//书名
 	private int bookCounts;
 	private String detail;
 	
