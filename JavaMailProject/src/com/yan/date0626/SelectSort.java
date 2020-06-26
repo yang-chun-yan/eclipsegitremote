19cf2dd381e2198213d7dbf6fb31d28e5b9bd7d5
ycy <3240056286@qq.com>
2020-06-26 15:49:22 +0800

    解决冲突

--- JavaMailProject/src/com/yan/date0626/SelectSort.java
+++ JavaMailProject/src/com/yan/date0626/SelectSort.java
@@ -7,7 +7,7 @@ package com.yan.date0626;
 */
 public class SelectSort {
    public static void main(String[] args) {
-	int[]array={3,2,5,1,4};
+	int[]array={3,2,5,1,4,8};
 	 for(int i=0;i<array.length-1;i++){
 		 for(int j=i+1;j<array.length;j++){
 			 if(array[i]>array[j]){
