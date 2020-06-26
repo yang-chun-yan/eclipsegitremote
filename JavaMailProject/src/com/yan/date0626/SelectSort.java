75354435ceaad5622fc0884e2f37207e7cb6fec1
yang-chun-yan <67322738+yang-chun-yan@users.noreply.github.com>
2020-06-26 16:02:00 +0800

    Update SelectSort.java
    更新

--- JavaMailProject/src/com/yan/date0626/SelectSort.java
+++ JavaMailProject/src/com/yan/date0626/SelectSort.java
@@ -10,7 +10,7 @@ public class SelectSort {
 	int[]array={3,2,5,1,4,6,8};
 	 for(int i=0;i<array.length-1;i++){
 		 for(int j=i+1;j<array.length;j++){
-			 if(array[i]>array[j]){
+			 if(array[i]<array[j]){
 				 int w=array[i];
 				 array[i]=array[j];
 				 array[j]=w;
